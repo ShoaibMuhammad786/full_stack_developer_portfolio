c8bdcfa8bad3d247ac01397b928a53fdfdd48793
Shoaib Muhammad <smshoaib2001@gmail.com>
2025-09-09 20:43:16 +0500

    initial commit

--- components/BookCall/BookCallPage.jsx
+++ components/BookCall/BookCallPage.jsx
@@ -1,15 +1,19 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { PopupButton } from "react-calendly";
+import dynamic from "next/dynamic";
+
+const PopupButton = dynamic(
+  () => import("react-calendly").then(mod => mod.PopupButton),
+  { ssr: false } // 🚀 disable server-side rendering for this import
+);
 
 const BookCallPage = () => {
-  const [root, setRoot] = useState(null);
+  const [root, setRoot] = useState<HTMLElement | null>(null);
 
   useEffect(() => {
-    if (typeof document !== "undefined") {
-      setRoot(document.body);
-    }
+    setRoot(document.body);
   }, []);
+
   return (
     <div className="w-full flex flex-col items-start justify-center gap-5 relative padding-x pb-20 pt-32 lg:pb-32 lg:pt-40 2xl:pt-52">
       <div className="w-full mt-10">
