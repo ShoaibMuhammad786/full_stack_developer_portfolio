e6204837e4b7bcdbece7341cd0ec68cf3b72dd0b
Shoaib Muhammad <smshoaib2001@gmail.com>
2025-09-09 20:39:00 +0500

    initial commit

--- components/BookCall/BookCallPage.jsx
+++ components/BookCall/BookCallPage.jsx
@@ -1,19 +1,26 @@
 "use client";
-import React from "react";
-import { InlineWidget } from "react-calendly";
+import React, { useEffect, useState } from "react";
 import { PopupButton } from "react-calendly";
 
 const BookCallPage = () => {
+  const [root, setRoot] = useState(null);
+
+  useEffect(() => {
+    if (typeof document !== "undefined") {
+      setRoot(document.body);
+    }
+  }, []);
   return (
     <div className="w-full flex flex-col items-start justify-center gap-5 relative padding-x pb-20 pt-32 lg:pb-32 lg:pt-40 2xl:pt-52">
       <div className="w-full mt-10">
-        {/* <InlineWidget url="https://calendly.com/smshoaib2001/project-discussion" /> */}
-        <PopupButton
-          url="https://calendly.com/smshoaib2001/project-discussion"
-          rootElement={document.body}
-          text="Book a Call"
-          className="bg-black text-white inline-block text-xs md:text-base lg:text-lg px-5 lg:px-8 py-4 rounded-3xl font-semibold"
-        />
+        {root && (
+          <PopupButton
+            url="https://calendly.com/smshoaib2001/project-discussion"
+            rootElement={root}
+            text="Book a Call"
+            className="bg-black text-white inline-block text-xs md:text-base lg:text-lg px-5 lg:px-8 py-4 rounded-3xl font-semibold"
+          />
+        )}
       </div>
     </div>
   );
